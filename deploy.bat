4ecefc0afb65db3e44ab75bbd4482d8e1ec017f1
Steven Feaster <stevenfeaster@mbp1qk64c.gateway.2wire.net>
2012-10-08 00:13:42 -0700

    adding deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,47 @@
+@ECHO OFF
+
+REM make sure we don't stomp variables in calling script
+SETLOCAL
+
+REM the location of this batch file
+SET SOURCE="%~DP0"
+
+REM target location
+SET TARGET=%1
+
+IF [%TARGET%]==[] GOTO FAIL
+
+GOTO OK
+
+:FAIL
+
+ECHO Must supply target folder parameter, e.g.:
+ECHO.`
+ECHO   deploy.bat ../deploy/lib/g11n
+ECHO.
+GOTO :EOF
+
+:OK
+
+REM copy all files
+pushd %SOURCE%
+xcopy %SOURCE%source %TARGET%\source\ /q /e 
+popd
+
+REM recursively go through target directories & remove non-data directories
+pushd %TARGET%
+for /d /r %%d in (*) do (
+ set "TRUE="
+ if "%%~nd"=="javascript" set TRUE=1 
+ if "%%~nd"=="loc" set TRUE=1
+ if "%%~nd"=="css" set TRUE=1
+ if defined TRUE (
+  rd "%%d" /s /q
+ )
+
+ REM remove package files
+ if exist "%%d\package.js" del "%%d\package.js"
+)
+popd
+
+ENDLOCAL
